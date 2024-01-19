f22164f5abaaf455e16ec5a24063b99743d5c828
brunovercosa <bruno.nv@hotmail.com>
2024-01-19 11:14:36 -0300

    Código inicial

--- index.js
+++ index.js
@@ -2,58 +2,30 @@ $(document).ready(function () {
     var expression = '';
 
     $('.clear').click(function () {
-        clearInput();
+        $('.input').text('');
+        expression = '';
+        result = '';
+        $('h2').text("Resultado = " + result);
     });
 
     $('.equal').click(function () {
-        evaluateExpression();
-    });
-
-    $('.multiply, .divide, .minus, .plus').click(function () {
-        handleOperatorInput($(this).text());
-    });
-
-    $('.one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .decimal, .zero').click(function () {
-        handleNumberInput($(this).text());
-    });
-
-    function clearInput() {
-        $('.input').text('');
-        expression = '';
-        $('h2').text("Result = ");
-    }
-
-    function evaluateExpression() {
         expression = $('.input').text();
         try {
             var result = eval(expression);
             $('h2').text("Result = " + result);
         } catch (error) {
-            alert('Invalid expression');
+            alert('Expressão inválida');
         }
-    }
+    });
 
-    function handleOperatorInput(operator) {
-        expression += operator;
+    $('.multiply, .divide, .minus, .plus').click(function () {
+        expression += $(this).text();
         $('.input').text(expression);
-    }
+    });
 
-    function handleNumberInput(number) {
-        if (number === '0' && expression === '0') {
-            // Não faça nada se houver zeros consecutivos
-            return;
-        }
+    $('.one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .decimal').click(function () {
+        expression += $(this).text();
+        $('.input').text(expression);
+    });
 
-        //Verifique se a entrada é zero e se já existem zeros no início
-        if (number === '0' && expression.length === 0) {
-            // Não faça nada se já houver zeros no início
-            if (!expression.startsWith('0')) {
-                expression += number;
-                $('.input').text(expression);
-            }
-        } else {
-            expression += number;
-            $('.input').text(expression);
-        }
-    }
 });
